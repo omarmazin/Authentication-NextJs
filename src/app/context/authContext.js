6fc7a35a286f58e5608461f76ae363faef8b8abd
omarmazin <omarbawazir42@gmail.com>
2023-09-20 00:46:14 +0300

    redirection add

--- src/app/context/authContext.js
+++ src/app/context/authContext.js
@@ -44,6 +44,7 @@ export const AuthContextProvider = ({children})=>{
             showConfirmButton: false,
             timer: 1500
           })
+          window.location.href = "/"
 
           
       
